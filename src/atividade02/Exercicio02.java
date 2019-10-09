c8a72ea0ed44016ed260358c0409d53bfaa4ea89
milton felga <milton.felga@etec.sp.gov.br>
2019-10-08 22:17:58 -0300

    continuando exercicio02

--- src/atividade02/Exercicio02.java
+++ src/atividade02/Exercicio02.java
@@ -1,5 +1,26 @@
 package atividade02;
 
+import javax.swing.JOptionPane;
+
+
 public class Exercicio02 {
-    
+    public static void main(String[] args) {
+        int i,j,media;
+        int notas [][]=new int[4][4];
+        String aux = null;
+        
+        for(i=1;i<=6;i++){
+            for(j=1;j<=2;j++){
+                System.out.println(i);
+                System.out.println(j);
+                aux = JOptionPane.showInputDialog("Aluno "+i+ " ,nota "+j);
+                notas[i][j]=Integer.parseInt(aux);
+                
+                media=(i+j)/2;
+                JOptionPane.showMessageDialog(null, media);
+        
+            }
+        }
+        
+    }   
 }
